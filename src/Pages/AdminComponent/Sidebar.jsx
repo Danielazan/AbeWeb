7fe3f691beeed63bc125149d9a73dfa85108c419
Akpa Onyedikachukwu <akpa3220@gmail.com>
2022-11-17 16:10:00 +0100

    Push

--- src/Pages/AdminComponent/Sidebar.jsx
+++ src/Pages/AdminComponent/Sidebar.jsx
@@ -3,14 +3,33 @@ import {Button,Offcanvas,ListGroup} from "react-bootstrap"
 import { MdSettingsSuggest,MdLocalActivity,MdRoofing } from "react-icons/md"
 import {FaHandHoldingWater} from "react-icons/fa"
 import {RiVideoAddLine} from "react-icons/ri"
+import axios from 'axios'
+import NewContext from "Hook/useCollection"
+
 
 
 function SideBar2() {
+  const {state,dispatch} = NewContext()
+
 
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    function getp(){
+
+      axios.get("https://abe-api.onrender.com/api/products/roofing")
+          .then(res=>{
+
+          console.log(res.data.materialss)
+
+          dispatch({type:"ROOFING",payload:res.data.materialss})
+
+          console.log(dispatch.payload)
+
+      })
+  }
   
   return (
     <React.Fragment>
@@ -22,10 +41,12 @@ function SideBar2() {
         <Offcanvas.Header closeButton >
           <Offcanvas.Title  >Settings</Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body   style={{backgroundColor:"#fda07e"}} >
+        <Offcanvas.Body   style={{backgroundColor:"#210440"}} >
         
           <ListGroup  className="SideList mt-4" variant='flush'> 
-              <ListGroup.Item style={{backgroundColor:"#210440",color:"#fda07e"}} className="border-white my-2 py-3 p-0 ms-lg-3 list" action ><MdRoofing className='me-2' size={"1.5em"}/>Roofing Collection</ListGroup.Item>
+              <ListGroup.Item style={{backgroundColor:"#210440",color:"#fda07e"}} className="border-white my-2 py-3 p-0 ms-lg-3 list" action onClick={getp} >
+                <MdRoofing className='me-2' size={"1.5em"} />Roofing Collection
+              </ListGroup.Item>
 
              
               <ListGroup.Item  style={{backgroundColor:"#210440",color:"#fda07e"}} className="border-white my-2 py-3 p-0 ms-lg-3 list" action ><FaHandHoldingWater className='me-2' size={"1.5em"}/>Water Collection</ListGroup.Item>
