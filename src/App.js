c47c0fdbfd05a47ad92ed6d2cb8a6e0d33c6951e
Lazan <danfrancix@gmail.com>
2022-11-20 16:22:49 -0800

    pages working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import {Container} from "react-bootstrap"
-import {Route,Routes,BrowserRouter} from "react-router-dom"
+import {Route,Routes,BrowserRouter, Navigate} from "react-router-dom"
 import Home from "Pages/HomeComponent/Home"
 import Login from "Pages/LoginComponent/Login"
 import Signup from "Pages/SignupComponent/Signup"
@@ -10,18 +10,29 @@ import About from "Pages/AdminComponent/Admin"
 import Sales from "Pages/SalesComponent/Sales"
 import Verify from "Pages/VerificationComponent/Verify"
 
+import UserHook from "Hook/UserHook"
+
 function App() {
+  const {User} = UserHook()
+
+  
   return (
     <div fluid className="App bg-white">
       <BrowserRouter>
           <Routes>
             <Route path='/' element={<Home/>}/>
-            <Route path='/Login' element={<Login/>}/>
-            <Route path='/Signup' element={<Signup/>}/>
-            <Route path='/Verify' element={<Verify/>}/>
-            <Route path='/Admin' element={<Admin/>}/>
+
+            <Route path='/Login' element={!User ? <Login/> : <Navigate to='/' />}/>
+            
+            <Route path='/Signup' element={ !User ? <Signup/> : <Navigate to='/' />}/>
+
+            <Route path='/Verify' element={ <Verify/>}/>
+
+            <Route path='/Admin' element={User ? <Admin/> : <Navigate to='/Login' />}/>
+
             <Route path="/about" element={<About/>}/>
-            <Route path="/Sales" element={<Sales/>}/>
+
+            <Route path="/Sales" element={User ? <Sales/> : <Navigate to='/Login' />}/>
           </Routes>
       </BrowserRouter>
     </div>
