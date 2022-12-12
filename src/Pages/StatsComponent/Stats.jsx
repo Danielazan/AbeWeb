270f3bf7e02be3f432af0fc3dae22e0eb6b2b3fb
Akpa Onyedikachukwu <akpa3220@gmail.com>
2022-12-12 20:55:45 +0100

    Stats2

--- src/Pages/StatsComponent/Stats.jsx
+++ src/Pages/StatsComponent/Stats.jsx
@@ -3,36 +3,77 @@ import { Container, Row, Col, Button, Form } from "react-bootstrap";
 import "./Style/Style.css";
 import Navbar from "Components/NavbarSales";
 
-
 function Stats() {
   return (
     <React.Fragment>
       <Container fluid className='Stats'>
         <Navbar />
         <Container>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          tempore hic sit neque corporis iure delectus facere accusamus
-          consequuntur tenetur aliquam unde minima illo, sed repudiandae ipsa,
-          labore vitae doloribus? Similique error quas at quibusdam laboriosam
-          minima numquam, consectetur accusantium! Iure laudantium suscipit quis
-          quasi doloribus, ratione esse nam ab maiores maxime magni mollitia
-          magnam odit alias rem delectus ex quod deserunt asperiores molestias
-          iusto sed. Accusantium quas impedit saepe neque ullam libero ab
-          repellat temporibus pariatur expedita error officiis beatae, porro
-          mollitia quidem doloremque quasi, suscipit consequatur recusandae
-          consectetur officia ut? Quibusdam, qui numquam. Assumenda repellendus
-          odit vel placeat nobis at ipsam dolores, cum accusantium vitae sit
-          deleniti praesentium tenetur? Officiis ullam odit quam explicabo sed,
-          aut, eum sequi doloremque sit distinctio cum iure magnam consequatur
-          tempora, possimus molestias. Aliquam eos alias possimus porro corporis
-          quos distinctio magni perferendis eum architecto, sunt maxime labore
-          dolor iste voluptatibus, quisquam ratione debitis ab, ex autem libero
-          voluptates nihil ipsum! Repellendus quam necessitatibus, natus beatae
-          labore tempora fugiat incidunt quaerat! Atque tenetur eum sapiente et
-          laboriosam cum dolores, animi nihil adipisci doloribus quos blanditiis
-          libero quae praesentium excepturi soluta non provident? Vel illum
-          corporis obcaecati nulla architecto. Temporibus voluptatum hic quasi
-          quas.
+          <h2 className='Stats-h2'>Stats</h2>
+
+          <Row
+            style={{
+              borderBlock: "3px solid #2e180e",
+              fontWeight: "600",
+            }}
+            className='py-3 mt-3'
+          >
+            <Col className='text-center' xs={2}>
+              Date
+            </Col>
+
+            <Col className='text-center' xs={2}>
+              Reg No
+            </Col>
+
+            <Col className='text-center' xs={3}>
+              Stock In
+            </Col>
+
+            <Col className='text-center' xs={3}>
+              Stock Out
+            </Col>
+
+            <Col className='text-center' xs={2}>
+              Balance
+            </Col>
+          </Row>
+
+          <Row className='py-4 tb-row'>
+            <Col className='text-center' xs={2}>
+              01/11/22
+            </Col>
+
+            <Col className='text-center' xs={2}>
+              001
+            </Col>
+
+            <Col className='text-center' xs={3}>
+              3000
+            </Col>
+
+            <Col className='text-center' xs={3}></Col>
+
+            <Col className='text-center' xs={2}></Col>
+          </Row>
+
+          <Row className='py-4 tb-row'>
+            <Col className='text-center' xs={2}>
+              02/11/22
+            </Col>
+
+            <Col className='text-center' xs={2}>
+              0456
+            </Col>
+
+            <Col className='text-center' xs={3}>
+             
+            </Col>
+
+            <Col className='text-center' xs={3}>20</Col>
+
+            <Col className='text-center' xs={2}>298</Col>
+          </Row>
         </Container>
       </Container>
     </React.Fragment>
