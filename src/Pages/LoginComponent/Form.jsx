01960387ea1e5bb0be90dfddcd1df0836f73105c
Akpa Onyedikachukwu <akpa3220@gmail.com>
2023-01-03 12:53:46 +0100

    Loading Icon

--- src/Pages/LoginComponent/Form.jsx
+++ src/Pages/LoginComponent/Form.jsx
@@ -4,6 +4,7 @@ import {Container,Row,Col,Button,Form} from "react-bootstrap"
 import {Link} from "react-router-dom"
 import "./Style/Style.css"
 import axios from "axios"
+import pic from "Assets/Images/pic17.svg"
 import UserHook from "Hook/UserHook"
 import { useNavigate } from 'react-router-dom';
 
@@ -67,61 +68,105 @@ function From() {
     }
   return (
     <React.Fragment>
-        <Row>
-            <Col xs={12} lg={3}></Col>
-            <Col xs={12} lg={6}>
-                <Container>
-                    <h1 className='form-h1'>Welcome Back</h1>
-                    <p className='form-p'>Welcome back! Please enter your details</p>
+      <Row>
+        <Col xs={12} lg={3}></Col>
+        <Col xs={12} lg={6}>
+          <Container>
+            <h1 className='form-h1'>Welcome Back</h1>
+            <p className='form-p'>Welcome back! Please enter your details</p>
 
-                    <Form>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label className='form-label'>Email </Form.Label>
+            <Form>
+              <Form.Group
+                className='mb-3'
+                controlId='exampleForm.ControlInput1'
+              >
+                <Form.Label className='form-label'>Email </Form.Label>
 
-                            <Form.Control type="email" placeholder="Enter Your Email" 
-                            value={email}
-                            onChange={(e)=>setEmail(e.target.value)}
-                            />
-                        </Form.Group>
+                <Form.Control
+                  type='email'
+                  placeholder='Enter Your Email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label className='form-label'>Password </Form.Label>
-                            <Form.Control type="password" placeholder="Password"
-                            value={password} 
-                            onChange={(e)=>setPassword(e.target.value)}
-                            />
-                        </Form.Group>
+              <Form.Group
+                className='mb-3'
+                controlId='exampleForm.ControlInput1'
+              >
+                <Form.Label className='form-label'>Password </Form.Label>
+                <Form.Control
+                  type='password'
+                  placeholder='Password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
 
-                        <Container fluid className='d-flex justify-content-between'>
-                            <Form.Check style={{fontSize:"12px"}} type="checkbox" id="custom-switch" label="Remember For 30 days"/>
+              <Container fluid className='d-flex justify-content-between'>
+                <Form.Check
+                  style={{ fontSize: "12px" }}
+                  type='checkbox'
+                  id='custom-switch'
+                  label='Remember For 30 days'
+                />
 
-                            <Link Navigate to="/" className='form-label' style={{fontSize:"12px"}}>Forgot Password</Link>                    
-                        </Container>
+                <Link
+                  Navigate
+                  to='/'
+                  className='form-label'
+                  style={{ fontSize: "12px" }}
+                >
+                  Forgot Password
+                </Link>
+              </Container>
 
-                        <Button
-                        disabled={isLoading} 
-                        variant='dark' className='w-100 mt-5'
-                        onClick={handleSubmit}
-                        >
-                        Sign In
-                        </Button>
-                        {Error &&
-                        <p style={{backgroundColor:"rgb(248, 215, 218)",color:"rgb(132, 32, 41)",border:"2px solid rgb(245, 194, 199)"}} 
-                        className='py-2 rounded-2 ps-2 mt-3'>
-                        {Error}
-                        </p>}
-                    </Form>
-                    <Container className='d-flex justify-content-around my-4'>
-                        <p className='form-p'>Don't have an account?</p>
+              <center>
+                <img className={` ${isLoading ? "d-block" : "d-none" }`} src={pic} alt='Logging In...' height='100px' />
+              </center>
 
-                        <Link Navigate style={{textDecoration:"none",fontWeight:"600",color:"black"}} to="/">Sign Up for Free</Link>
-                    </Container>
-                </Container>
-            </Col>
-            <Col xs={12} lg={3}></Col>
-        </Row>
+              <Button
+                disabled={isLoading}
+                variant='dark'
+                className='w-100'
+                onClick={handleSubmit}
+              >
+                Sign In
+              </Button>
+              {Error && (
+                <p
+                  style={{
+                    backgroundColor: "rgb(248, 215, 218)",
+                    color: "rgb(132, 32, 41)",
+                    border: "2px solid rgb(245, 194, 199)",
+                  }}
+                  className='py-2 rounded-2 ps-2 mt-3'
+                >
+                  {Error}
+                </p>
+              )}
+            </Form>
+            <Container className='d-flex justify-content-around my-4'>
+              <p className='form-p'>Don't have an account?</p>
+
+              <Link
+                Navigate
+                style={{
+                  textDecoration: "none",
+                  fontWeight: "600",
+                  color: "black",
+                }}
+                to='/'
+              >
+                Sign Up for Free
+              </Link>
+            </Container>
+          </Container>
+        </Col>
+        <Col xs={12} lg={3}></Col>
+      </Row>
     </React.Fragment>
-  )
+  );
 
 }  
 export default From
